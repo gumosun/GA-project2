a06890b84e60e9256b910b9a55c1f7a745f5ff53
Hao Sun <hao.sun@nyu.edu>
2017-08-06 14:21:52 -0400

    CSS modified

--- db/config.js
+++ db/config.js
@@ -1,42 +1,42 @@
-const options = {
-  query: (e) => {
-    console.log(e.query);
-  },
-};
+// const options = {
+//   query: (e) => {
+//     console.log(e.query);
+//   },
+// };
 
-const pgp = require('pg-promise')(options);
+// const pgp = require('pg-promise')(options);
 
-function setDatabase() {
-  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-    return pgp({
-      database: 'my_rating_dev',
-      port: 5432,
-      host: 'localhost',
-    });
-  } else if (process.env.NODE_ENV === 'production') {
-    return pgp(process.env.DATABASE_URL);
-  }
-}
+// function setDatabase() {
+//   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+//     return pgp({
+//       database: 'my_rating_dev',
+//       port: 5432,
+//       host: 'localhost',
+//     });
+//   } else if (process.env.NODE_ENV === 'production') {
+//     return pgp(process.env.DATABASE_URL);
+//   }
+// }
 
-const db = setDatabase();
+// const db = setDatabase();
 
-module.exports = db;
+// module.exports = db;
 
 
 //setting for heroku
 
-// const pgp = require('pg-promise')();
+const pgp = require('pg-promise')();
 
-// let db;
+let db;
 
-// if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-//   db = pgp({
-//     database: 'adaquote_development',
-//     port: 5432,
-//     host: 'localhost',
-//   });
-// } else if (process.env.NODE_ENV === 'production') {
-//   db = pgp(process.env.DATABASE_URL);
-// }
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+    database: 'adaquote_development',
+    port: 5432,
+    host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  db = pgp(process.env.DATABASE_URL);
+}
 
-// module.exports = db;
+module.exports = db;
